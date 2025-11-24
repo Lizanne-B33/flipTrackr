42a21f9683e667d9a9b5e70364c308432d678788
Lizanne-B33 <lizanne.b33@gmail.com>
2025-11-24 00:01:28 -0500

    A very painful login/logout experience

--- backend/my_django_app/flipTrackr/urls.py
+++ backend/my_django_app/flipTrackr/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     # Test React with Django
     path('api/hello/', hello_world),
     # Test JWT
-    path('home/', views.HomeView.as_view(), name='home'),
-    path('logout/', views.LogoutView.as_view(), name='logout')
+    path('api/home/', views.HomeView.as_view(), name='home'),
+    path('api/logout/', views.LogoutView.as_view(), name='logout')
 ]
